b4044cf0b98c80cf9980c0871d7f49218ea325a9
Rennan Bacili <rennanbaccili@gmail.com>
2023-09-26 16:22:55 -0300

    Pequena Atualização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './home/main/main.component';
+import { AboutComponent } from './home/about/about.component';
+import { DocesComponent } from './home/doces/doces.component';
+import { ContatoComponent } from './home/contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    MainComponent,
+    AboutComponent,
+    DocesComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/doces/doces.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doces',
+  templateUrl: './doces.component.html',
+  styleUrls: [
+    './doces.component.css',
+  './doces.responsive.component.css']
+})
+export class DocesComponent {
+
+}
